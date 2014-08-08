5814b4d6a6d9b8a6400606fa8153d60cbe25b699
Ken Pierce <kpierce8@gmail.com>
2014-08-08 16:59:26 -0700

    still trying to get button to respond

--- ComboBindTest/MainWindow.xaml.cs
+++ ComboBindTest/MainWindow.xaml.cs
@@ -24,5 +24,7 @@ namespace ComboBindTest
         {
             InitializeComponent();
         }
+
+      
     }
 }
---
+++ ComboBindTest/Model/ButtonChromeStyleSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ComboBindTest.Model
+{
+    class ButtonChromeStyleSelector: StyleSelector
+    {
+
+        public Style ButtonStyle1 { get; set; }
+        public Style ButtonStyle2 { get; set; }
+
+        public override Style SelectStyle(object item,
+        DependencyObject container)
+        {
+            Style st = new Style();
+
+            Button thisButton = (Button)item;
+
+            int index2 = Int16.Parse((string)thisButton.Tag);
+            int index = Int16.Parse((string)thisButton.CommandParameter);
+
+           
+            return ButtonStyle1;
+        }
+    }
+}
